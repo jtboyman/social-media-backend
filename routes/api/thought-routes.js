98e5cbac74fec9e6b3c941770890d7ea10bff4a0
Edelgard <jtboyman@gmail.com>
2022-01-31 17:17:50 -0500

    complete all routes and tested

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -14,6 +14,9 @@ router.route('/:thoughtId').get(getThoughtById).put(updateThought);
 router.route('/:userId/:thoughtId').delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').put(addReaction).delete(deleteReaction);
+router.route('/:thoughtId/reactions').put(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
